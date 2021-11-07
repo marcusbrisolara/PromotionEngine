95ddf8417f14601728079557bb2071ecb6d46179
Marcus Brisolara <Marcus.Brisolara@derivco.co.uk>
2021-11-07 22:20:38 +0000

    Adding entities(now it's true)

---
+++ Promotion.UI/Entities/AppliedPromotion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Promotion.UI.Entities
+{
+    public class AppliedPromotion
+    {
+        public AppliedPromotion(string description, decimal discount)
+        {
+            this.Description = description;
+            this.Discount = discount;
+        }
+        public string Description { get; set; }
+        public decimal Discount { get; set; }
+    }
+}
---
+++ Promotion.UI/Entities/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Promotion.UI.Entities
+{
+    public class Cart
+    {
+        public List<CartItem> CartItems = new List<CartItem>();
+        public List<AppliedPromotion> AppliedPromotions = new List<AppliedPromotion>();
+        public decimal OriginalTotalPrice => CartItems?.Sum(x => x.TotalPrice) ?? 0;
+        public decimal FinalTotalPrice => OriginalTotalPrice - (AppliedPromotions.Sum(x => x.Discount));
+    }
+}
