d62fb4158c2c1ce5e7bdc21d2230127ec39bdea0
Marcus Brisolara <Marcus.Brisolara@derivco.co.uk>
2021-11-07 22:56:05 +0000

    Adding the generic rule for N Items for a fixed price. Adding the N Items for a fixed price parameter entity

---
+++ Promotion.UI/Entities/NItemsFixedPriceParameters.cs
@@ -0,0 +1,13 @@
+﻿namespace Promotion.UI.Entities
+{
+    public class NItemsFixedPriceParameters
+    {
+        public int NumberOfItems { get; set; }
+
+        public char ItemId { get; set; }
+
+        public decimal FixedPrice { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ Promotion.UI/Rules/NItemsFixedPrice.cs
@@ -0,0 +1,56 @@
+﻿using Promotion.UI.Entities;
+using Promotion.UI.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Promotion.UI.Rules
+{
+    public class NItemsFixedPrice : IRule
+    {
+        private IEnumerable<NItemsFixedPriceParameters> _promotions;
+
+        public NItemsFixedPrice()
+        {
+            _promotions = new List<NItemsFixedPriceParameters>
+            {
+                new NItemsFixedPriceParameters 
+                { 
+                    NumberOfItems = 3,
+                    ItemId = 'A',
+                    FixedPrice = 130,
+                    Description = "3 A's for 130"
+                },
+                new NItemsFixedPriceParameters
+                {
+                    NumberOfItems = 2,
+                    ItemId = 'B',
+                    FixedPrice = 45,
+                    Description = "2 B's for 45"
+                }
+            };
+        }   
+
+        public Cart Apply(Cart cart)
+        {
+            foreach (var promotion in _promotions)
+            {
+                if (ShouldApply(cart, promotion))
+                {
+                    var itemPromotion = cart.CartItems.FirstOrDefault(x => x.Id == promotion.ItemId);
+                    var qtyPromotionPrice = itemPromotion.Quantity / promotion.NumberOfItems;
+                    var qtyNormalPrice = itemPromotion.Quantity - (qtyPromotionPrice * promotion.NumberOfItems);
+
+                    var discount = itemPromotion.TotalPrice - ((qtyPromotionPrice * promotion.FixedPrice) + qtyNormalPrice * itemPromotion.UnitPrice);
+
+                    cart.AppliedPromotions.Add(new AppliedPromotion(promotion.Description, discount));
+                }
+            }
+            return cart;
+        }
+
+        private bool ShouldApply(Cart cart, NItemsFixedPriceParameters promotion)
+        {
+            return cart.CartItems.FirstOrDefault(x => x.Id == promotion.ItemId).Quantity >= promotion.NumberOfItems;
+        }
+    }
+}
