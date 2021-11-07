3b74a41a3181888a6a02709b42bfd21abaa09fd3
Marcus Brisolara <Marcus.Brisolara@derivco.co.uk>
2021-11-07 21:56:47 +0000

    Add project files.

---
+++ Promotion.UI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Promotion.UI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PromotionsController : ControllerBase
+    {
+        private readonly ILogger<PromotionsController> _logger;
+
+        public PromotionsController(ILogger<PromotionsController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
