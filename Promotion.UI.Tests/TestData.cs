5ea51dee330afa3530e4fdeb6ce248273a639b34
Marcus Brisolara <Marcus.Brisolara@derivco.co.uk>
2021-11-07 22:16:44 +0000

    Adding entities and unit tests

---
+++ Promotion.UI.Tests/TestData.cs
@@ -0,0 +1,51 @@
+﻿using Promotion.UI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promotion.UI.Tests
+{
+    public static class TestData
+    {
+        internal static Cart ScenarioA_Cart = new Cart
+        {
+            CartItems = ScenarioA_CartItems
+        };
+
+        internal static Cart ScenarioB_Cart = new Cart
+        {
+            CartItems = ScenarioB_CartItems
+        };
+
+        internal static Cart ScenarioC_Cart = new Cart
+        {
+            CartItems = ScenarioC_CartItems
+        };
+
+        internal static Cart EmptyCart = new Cart();
+
+        static List<CartItem> ScenarioA_CartItems => new List<CartItem>
+        {
+            new CartItem{ Id = 'A', Quantity = 1 , UnitPrice = 50 },
+            new CartItem{ Id = 'B', Quantity = 1 , UnitPrice = 30 },
+            new CartItem{ Id = 'C', Quantity = 1 , UnitPrice = 20 }
+        };
+
+        static List<CartItem> ScenarioB_CartItems => new List<CartItem>
+        {
+            new CartItem{ Id = 'A', Quantity = 5 , UnitPrice = 50 },
+            new CartItem{ Id = 'B', Quantity = 5 , UnitPrice = 30 },
+            new CartItem{ Id = 'C', Quantity = 1 , UnitPrice = 20 }
+        };
+
+        static List<CartItem> ScenarioC_CartItems => new List<CartItem>
+        {
+            new CartItem{ Id = 'A', Quantity = 3 , UnitPrice = 50 },
+            new CartItem{ Id = 'B', Quantity = 5 , UnitPrice = 30 },
+            new CartItem{ Id = 'C', Quantity = 1 , UnitPrice = 20 },
+            new CartItem{ Id = 'D', Quantity = 1 , UnitPrice = 15 }
+        };
+    }
+}
--- Promotion.UI.Tests/UnitTest1.cs
+++ Promotion.UI.Tests/UnitTest1.cs
@@ -10,9 +10,39 @@ namespace Promotion.UI.Tests
         }
 
         [Test]
-        public void Test1()
+        public void ScenarioA()
         {
-            Assert.Pass();
+            PromotionEngineService engineService = new PromotionEngineService();
+            var cart = engineService.ProcessPromotions(TestData.ScenarioA_Cart);
+
+            Assert.AreEqual(100, cart.FinalTotalPrice);
+        }
+
+        [Test]
+        public void ScenarioB()
+        {
+            PromotionEngineService engineService = new PromotionEngineService();
+            var cart = engineService.ProcessPromotions(TestData.ScenarioB_Cart);
+
+            Assert.AreEqual(370, cart.FinalTotalPrice);
+        }
+
+        [Test]
+        public void ScenarioC()
+        {
+            PromotionEngineService engineService = new PromotionEngineService();
+            var cart = engineService.ProcessPromotions(TestData.ScenarioC_Cart);
+
+            Assert.AreEqual(280, cart.FinalTotalPrice);
+        }
+
+        [Test]
+        public void EmptyCart()
+        {
+            PromotionEngineService engineService = new PromotionEngineService();
+            var cart = engineService.ProcessPromotions(TestData.EmptyCart);
+
+            Assert.AreEqual(0, cart.FinalTotalPrice);
         }
     }
 }
